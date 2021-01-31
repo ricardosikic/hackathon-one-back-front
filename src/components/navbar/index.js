c24bab28c2e6853ad31434ca0461a4e15af228d7
Ricardo <ricardo.sikic@gmai.com>
2021-01-30 22:12:16 -0300

    :art: navbar images

---
+++ src/components/navbar/index.js
@@ -0,0 +1,40 @@
+// React
+import React from 'react';
+
+// React-bootstrap
+import { 
+    Navbar, 
+    Nav, 
+    NavDropdown, 
+    Form, 
+    FormControl, 
+    Button 
+} from 'react-bootstrap';
+
+// Style
+import './style.sass';
+
+// Images
+import heart from '../../assets/images/logo/heart-home.png';
+
+class NavBar extends React.Component {
+    render() {
+        return(
+            <Navbar bg="transparent" className="navbar-container" expand="lg">
+                <Navbar.Brand href="#home"><img className="logo" src={heart} />BOOKS</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                    <Nav.Link href="#home">HOME</Nav.Link>
+                    <Nav.Link href="#link">EXPLORE BOOKS</Nav.Link>
+                    </Nav>
+                    <Form inline>
+                    <Button variant="outline-success">SIGN IN</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
+}
+
+export default NavBar;
